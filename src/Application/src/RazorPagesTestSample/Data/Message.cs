84d450d255a7de7f264dbd6e47e60f37b9e98804
pmedranos <paul.medranos@gmail.com>
2024-10-23 11:28:18 -0600

    Remove pages YAML fiel

--- src/Application/src/RazorPagesTestSample/Data/Message.cs
+++ src/Application/src/RazorPagesTestSample/Data/Message.cs
@@ -7,6 +7,15 @@ namespace RazorPagesTestSample.Data
     {
         public int Id { get; set; }
 
+        /// <summary>
+        /// Gets or sets the message text.
+        /// </summary>
+        /// <value>
+        /// The text of the message, which is required and must be a string with a maximum length of 200 characters.
+        /// </value>
+        /// <remarks>
+        /// An error message will be displayed if the text exceeds 200 characters: "There's a 200 character limit on messages. Please shorten your message."
+        /// </remarks>
         [Required]
         [DataType(DataType.Text)]
         [StringLength(200, ErrorMessage = "There's a 200 character limit on messages. Please shorten your message.")]
